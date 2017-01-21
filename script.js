98b4fc90885ccfc459aa60215f2ee2aea90d1b08
meharaj <meharaj.m@snapwiz.com>
2017-01-21 19:16:23 +0530

    added some more functionalies

--- script.js
+++ script.js
@@ -6,35 +6,46 @@ $(document).ready(function(){
 	}
 	$('#save').bind('click',addCat);
 	$('.cat').bind('click',showCat);
-	$('img').bind('click',increaseClick);
+	$('.details').on('click','img',increaseClick);
+	$('li').bind('click',showListCat);
+	function showListCat(ev){
+		$(".active").removeClass('active');
+		$(ev.currentTarget).addClass('active');
+		showCat(ev);
+	}
 	function showCat(ev){
-		var id = ev.currentTarget.id;
-		increaseClick(id);
+		var isListItemClicked = ev.currentTarget.id.indexOf('list') > -1 ? true : false;
+		var id = isListItemClicked ? ev.currentTarget.id.replace('list-','') : ev.currentTarget.id;
+		increaseClick(id,isListItemClicked);
+		id = ev.currentTarget.id.indexOf('list') > -1 ? ev.currentTarget.id.replace('list-','') : id;
 		var selectedCat = catList[parseInt(id)-1];
-		var detailTemplate = _.template("<h3><%= name%></h3><p>clicks <%= clk%></p><img src='<%= img%>' id='img-<%=id%>'/><p><%=names%></p><p><%=age%></p>");
+		var detailTemplate = _.template("<h3><%= name%></h3><p>clicks <span class='clk-<%=id%>'><%= clk%></span></p><img src='<%= img%>' id='img-<%=id%>'/><p><%=names%></p><p class='age-<%=id%>'><%=age%></p>");
 		$('#details').html(detailTemplate(selectedCat));
 		$('#name').val(selectedCat.name);
 		$('#image').val(selectedCat.img);
 		$('#clicks').val(selectedCat.clk);
 		$('#nick-names').val(selectedCat.names);
 	}
-	function increaseClick(id){
+	function increaseClick(id, isListItemClicked){
 		if(typeof id === "object"){
-			id = id.currentTarget.id.replace('img-','');
+			var ev = id;
+			id = ev.currentTarget.id.replace('img-','');
 		}
 		var selectedCat = catList[parseInt(id)-1];
-		selectedCat.clk = parseInt(selectedCat.clk) + 1;
-		selectedCat.age = getAge(selectedCat.clk);
-	}
-	function getAge(clk){
-		switch(clk){
-			case clk >= 0 && clk < 5: return "Infant";
-			case clk >=6 && clk < 12: return "Child";
-			case clk >=13 && clk < 25: return "Young";
-			case clk >=26 && clk < 40: return "Middle-Age";
-			case clk >= 41 && clk < 60: return "Old";
-			case clk >61: return "Very Old";
+		if(!isListItemClicked){
+			selectedCat.clk = parseInt(selectedCat.clk) + 1;		
 		}
+		selectedCat.age = getAge(selectedCat.clk);
+		localStorage.setItem("catList",JSON.stringify(catList));
+		updateInfo(id);
+	}
+	function getAge(clk){		
+			if(clk >= 0 && clk < 5) return "Infant";
+			if(clk >=6 && clk < 12) return "Child";
+			if(clk >=13 && clk < 25) return "Young";
+			if(clk >=26 && clk < 40) return "Middle-Age";
+			if(clk >= 41 && clk < 60) return "Old";
+			if(clk >61) return "Very Old";		
 	}
 	function addCat(){		
 		var name = $('#name').val();
@@ -58,7 +69,7 @@ $(document).ready(function(){
 		var galleryTemplate = _.template(" <div class='col-lg-3 col-sm-4 col-xs-6'>"+									
 										"<div class='col-xs-12 cat' id='<%=id%>''>"+
 											"<h3><%= name%></h3>"+
-											"<p>clicks <%= clk%></p>"+
+											"<p>clicks <span class='clk-<%=id%>'><%= clk%><span></p>"+
 											"<img src='<%= img%>' id='img-<%=id%>'/>"+
 										"</div>"+									
 								"</div>");
@@ -68,5 +79,12 @@ $(document).ready(function(){
 			gallery.append(galleryTemplate(cat));
 			cat_list.append(listTemplate(cat))
 		});
+	}
+	function updateInfo(id){
+		var selectedCat = catList[parseInt(id)-1];
+		$('#list-'+id).find('span').html(selectedCat.clk);
+		$('.clk-'+id).html(selectedCat.clk);
+		$('age-'+id).html(selectedCat.age);
+		$('#clicks').val(selectedCat.clk);
 	}
 });
